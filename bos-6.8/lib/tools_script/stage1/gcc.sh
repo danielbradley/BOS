a174bb054df584e1082fea25d16390f09ceec4d7
Daniel Bradley <daniel.bradley@islabs.com>
2019-07-14 16:33:26 +1000

    Modified gcc.sh - fixed where dependencies are extracted to.

--- bos-6.8/lib/tools_script/stage1/gcc.sh
+++ bos-6.8/lib/tools_script/stage1/gcc.sh
@@ -45,12 +45,12 @@ unpack_package()
 	then
 		mkdir -p $BUILD_DIR                                       &&
 		tar -C $BUILD_DIR -jxvf $SOURCE/$PACKAGE-$VERSION.tar.bz2 &&
-		tar -C $BUILD_DIR/$PACKAGE-$VERSION -jxvf $SOURCE/$MPF.tar.bz2              &&
-		tar -C $BUILD_DIR/$PACKAGE-$VERSION -jxvf $SOURCE/$GMP.tar.bz2              &&
-		tar -C $BUILD_DIR/$PACKAGE-$VERSION -zxvf $SOURCE/$MPC.tar.bz               &&
-		mv -v  $BUILD_DIR/$PACKAGE-$VERSION/$MPF $BUILD_DIR/$PACKAGE-$VERSION/mpf   &&
-		mv -v  $BUILD_DIR/$PACKAGE-$VERSION/$GMP $BUILD_DIR/$PACKAGE-$VERSION/gmp   &&
-		mv -v  $BUILD_DIR/$PACKAGE-$VERSION/$MPC $BUILD_DIR/$PACKAGE-$VERSION/mpc
+		tar -C $BUILD_DIR -jxvf $SOURCE/$MPF.tar.bz2              &&
+		tar -C $BUILD_DIR -jxvf $SOURCE/$GMP.tar.bz2              &&
+		tar -C $BUILD_DIR -zxvf $SOURCE/$MPC.tar.bz               &&
+		ln -sf ../$MPF $BUILD_DIR/$PACKAGE-$VERSION/mpfr          &&
+		ln -sf ../$MPF $BUILD_DIR/$PACKAGE-$VERSION/gmp           &&
+		ln -sf ../$MPF $BUILD_DIR/$PACKAGE-$VERSION/mpc
 	fi
 }
 
