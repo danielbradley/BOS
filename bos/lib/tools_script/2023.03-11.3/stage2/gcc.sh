79db13b6d2c757d311b9592b2282209ebbd9eeaf
Daniel Bradley <daniel@crossadaptive.com>
2023-04-10 12:10:38 +1000

    Added other existing dirs to tools_script/2023.03-11.3.

---
+++ bos/lib/tools_script/2023.03-11.3/stage2/gcc.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+#
+#       Copyright (c) 2004-2006 Daniel Robert Bradley. All rights reserved.
+#
+
+SOURCE=$BUILD_BASE/tools/source	# Where source packages are located
+BUILD=$BUILD_BASE/tools/stage2	# Where this package should be built
+
+PACKAGE=gcc						# Package information
+VERSION=4.0.3					# Version information
+
+GNU_PREFIX=/tools				# Prefix packages are installed into
+
+#CHOST=i386-pc-linux-gnu
+
+ARCHIVE=tar.bz2
+PKG_DIR=core/toolchain
+
+main()
+{
+	echo $PACKAGE-$VERSION
+
+	download ${PACKAGE}-${VERSION}.${ARCHIVE}    &&
+	download ${PACKAGE}-${VERSION}-specs-1.patch &&
+	unpack_package                               &&
+	apply_patches                                &&
+	configure_source                             &&
+	compile_source                               &&
+	install_package                              &&
+	complete                                     &&
+	echo done
+}
+
+unpack_package()
+{
+	if [ ! -d $BUILD/$PACKAGE-$VERSION ]
+	then
+		mkdir -p $BUILD
+		tar -C $BUILD -jxvf $SOURCE/$PACKAGE-$VERSION.tar.bz2
+	fi
+}
+
+apply_patches()
+{
+	if [ -f $BUILD/$PACKAGE-$VERSION/README ]
+	then
+		if [ ! -f $BUILD/$PACKAGE-$VERSION/SUCCESS.PATCHED ]
+		then
+			cd $BUILD/$PACKAGE-$VERSION
+			patch -Np1 -i $SOURCE/$PACKAGE-$VERSION-specs-1.patch &&
+			touch $BUILD/$PACKAGE-$VERSION/SUCCESS.PATCHED
+		fi
+	fi
+}
+
+configure_source()
+{
+	if [ -f $BUILD/$PACKAGE-$VERSION/SUCCESS.PATCHED ]
+	then
+		if [ ! -f $BUILD/$PACKAGE-$VERSION/SUCCESS.CONFIGURE ]
+		then
+			mkdir -p $BUILD/$PACKAGE-build &&
+			cd $BUILD/$PACKAGE-build &&
+
+#			CFLAGS="-march=i386"
+			../$PACKAGE-$VERSION/configure \
+				--prefix=$GNU_PREFIX \
+				--libexecdir=/tools/lib \
+				--with-local-prefix=/tools \
+				--enable-clocale=gnu \
+				--enable-shared \
+				--enable-threads=posix \
+				--enable-__cxa_atexit \
+				--enable-languages=c,c++ \
+				--disable-libstdcxx-pch
+#				--host=$CHOST --target=$CHOST &&
+			touch /$BUILD/$PACKAGE-$VERSION/SUCCESS.CONFIGURE
+		fi
+	fi
+}
+
+compile_source()
+{
+	if [ -f $BUILD/$PACKAGE-$VERSION/SUCCESS.CONFIGURE ]
+	then
+		if [ ! -f $BUILD/$PACKAGE-$VERSION/SUCCESS.MAKE ]
+		then
+			cd $BUILD/$PACKAGE-build &&
+			make &&
+			touch $BUILD/$PACKAGE-$VERSION/SUCCESS.MAKE
+		fi
+	fi
+}
+
+install_package()
+{
+	if [ -f $BUILD/$PACKAGE-$VERSION/SUCCESS.MAKE ]
+	then
+		if [ ! -f $BUILD/$PACKAGE-$VERSION/SUCCESS.INSTALL ]
+		then
+			cd $BUILD/$PACKAGE-build &&
+			make install &&
+			ln -sf gcc /tools/bin/cc &&
+			touch $BUILD/$PACKAGE-$VERSION/SUCCESS.INSTALL
+		fi
+	fi
+}
+
+complete()
+{
+	if [ -f $BUILD/$PACKAGE-$VERSION/SUCCESS.INSTALL ]
+	then
+		rm -rf $BUILD/$PACKAGE-$VERSION/*
+		rm -rf $BUILD/$PACKAGE-build/*
+	fi
+}
+
+premain()
+{
+	local Here=`pwd`
+	main $@
+	let ret=$?
+	cd ${Here}
+	return $ret
+}
+
+premain $@
