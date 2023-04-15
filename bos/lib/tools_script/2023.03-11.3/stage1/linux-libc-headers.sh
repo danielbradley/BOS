74e1e3c74b6f4ed687b73061429683fb9541304b
Daniel Bradley <daniel@crossadaptive.com>
2023-04-15 20:42:45 +1000

    Modified 2023.03-11.3/stage1/linux-libc-headers.sh - initial changes.

--- bos/lib/tools_script/2023.03-11.3/stage1/linux-libc-headers.sh
+++ bos/lib/tools_script/2023.03-11.3/stage1/linux-libc-headers.sh
@@ -92,8 +92,8 @@ install_package()
 		then
 			cd    $BUILD_DIR/$PACKAGE-$VERSION                &&
 			#cp -rv usr/include $GNU_PREFIX/usr               &&
-			cp -Rv include/asm-i386 $GNU_PREFIX/include/asm   &&
-			cp -Rv include/linux    $GNU_PREFIX/include/linux &&
+			cp -Rv include/asm-generic $GNU_PREFIX/include/asm   &&
+			cp -Rv include/linux       $GNU_PREFIX/include/linux &&
 			touch $BUILD_DIR/$PACKAGE-$VERSION/SUCCESS.INSTALL
 		fi
 	fi
