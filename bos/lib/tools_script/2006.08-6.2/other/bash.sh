11d20bbade9fa417cc2ba5cf284304c53ff632d1
Daniel Bradley <daniel@crossadaptive.com>
2024-09-25 21:00:01 +1000

    Modified bash.sh - fixed archive type.

--- bos/lib/tools_script/2006.08-6.2/other/bash.sh
+++ bos/lib/tools_script/2006.08-6.2/other/bash.sh
@@ -13,8 +13,9 @@ GNU_PREFIX=/tools		# Prefix packages are installed into
 
 #CHOST=i386-pc-linux-gnu
 
-ARCHIVE=tar.bz2
+ARCHIVE=tar.gz
 PKG_DIR=core/commands
+UNZIP=-z
 
 main()
 {
@@ -36,7 +37,7 @@ unpack_package()
 	if [ ! -d $BUILD/$PACKAGE-$VERSION ]
 	then
 		mkdir -p $BUILD
-		tar -C $BUILD -jxvf $SOURCE/$PACKAGE-$VERSION.tar.bz2
+		tar -C $BUILD -xvf $SOURCE/$PACKAGE-$VERSION.tar.bz2 $UNZIP
 	fi
 }
 
